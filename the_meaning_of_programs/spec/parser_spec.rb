daa97b648e59fc30c095aee7a252e0e4c1c66e0f
Tom Stuart <tom@codon.com>
2013-06-02 23:20:09 +0100

    Import remaining Simple grammar rules and parser specs

--- the_meaning_of_programs/spec/parser_spec.rb
+++ the_meaning_of_programs/spec/parser_spec.rb
@@ -9,6 +9,11 @@ describe 'the Simple parser' do
       specify { '42'.should parse_as Number.new(42) }
     end
 
+    describe 'booleans' do
+      specify { 'true'.should parse_as Boolean.new(true) }
+      specify { 'false'.should parse_as Boolean.new(false) }
+    end
+
     describe 'variables' do
       specify { 'x'.should parse_as Variable.new(:x) }
       specify { 'y'.should parse_as Variable.new(:y) }
@@ -24,6 +29,19 @@ describe 'the Simple parser' do
       specify { '1 * 2'.should parse_as Multiply.new(Number.new(1), Number.new(2)) }
       specify { '1 * 2 * 3'.should parse_as Multiply.new(Number.new(1), Multiply.new(Number.new(2), Number.new(3))) }
     end
+
+    describe 'addition' do
+      specify { '1 + 2'.should parse_as Add.new(Number.new(1), Number.new(2)) }
+      specify { '1 + 2 + 3'.should parse_as Add.new(Number.new(1), Add.new(Number.new(2), Number.new(3))) }
+    end
+
+    describe 'operator precedence' do
+      specify { '1 + 2 < 3'.should parse_as LessThan.new(Add.new(Number.new(1), Number.new(2)), Number.new(3)) }
+      specify { '1 < 2 + 3'.should parse_as LessThan.new(Number.new(1), Add.new(Number.new(2), Number.new(3))) }
+      specify { '1 + 2 * 3'.should parse_as Add.new(Number.new(1), Multiply.new(Number.new(2), Number.new(3))) }
+      specify { '1 + 2 < 3 * 4'.should parse_as LessThan.new(Add.new(Number.new(1), Number.new(2)), Multiply.new(Number.new(3), Number.new(4))) }
+      specify { '1 * 2 + 3'.should parse_as Add.new(Multiply.new(Number.new(1), Number.new(2)), Number.new(3)) }
+    end
   end
 
   describe 'statements' do
@@ -32,8 +50,22 @@ describe 'the Simple parser' do
       specify { 'y = 6 * 7'.should parse_as Assign.new(:y, Multiply.new(Number.new(6), Number.new(7))) }
     end
 
+    describe 'conditional' do
+      specify { 'if (x < 42) { x = 6 } else { x = 7 }'.should parse_as If.new(LessThan.new(Variable.new(:x), Number.new(42)), Assign.new(:x, Number.new(6)), Assign.new(:x, Number.new(7))) }
+      specify { 'if (true) { x = 6 } else { x = 7 }'.should parse_as If.new(Boolean.new(true), Assign.new(:x, Number.new(6)), Assign.new(:x, Number.new(7))) }
+    end
+
     describe 'while' do
       specify { 'while (x < 5) { x = x * 3 }'.should parse_as While.new(LessThan.new(Variable.new(:x), Number.new(5)), Assign.new(:x, Multiply.new(Variable.new(:x), Number.new(3)))) }
+    end
+
+    describe 'doing nothing' do
+      specify { 'do-nothing'.should parse_as DoNothing.new }
+    end
+
+    describe 'sequence' do
+      specify { 'do-nothing; do-nothing'.should parse_as Sequence.new(DoNothing.new(), DoNothing.new()) }
+      specify { 'x = 6; y = 7; z = x * y'.should parse_as Sequence.new(Assign.new(:x, Number.new(6)), Sequence.new(Assign.new(:y, Number.new(7)), Assign.new(:z, Multiply.new(Variable.new(:x), Variable.new(:y))))) }
     end
   end
 end
