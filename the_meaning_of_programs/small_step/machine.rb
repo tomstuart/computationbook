762506e8741ebb3509ea240a48f82963922e2958
Tom Stuart <tom@codon.com>
2013-06-09 13:57:45 +0100

    Reintroduce a Machine class for backwards compatibility
    Machine.new should continue to work, but with a warning about
    ExpressionMachine and StatementMachine.

---
+++ the_meaning_of_programs/small_step/machine.rb
@@ -0,0 +1,22 @@
+require_relative '../syntax'
+require_relative 'expression_machine'
+require_relative 'statement_machine'
+
+# This class is provided as a backwards-compatibility layer so that Machine.new continues to work.
+class Machine
+  def self.new(syntax, *args)
+    machine_class =
+      case syntax
+      when Add, Boolean, LessThan, Multiply, Number, Variable
+        ExpressionMachine
+      when Assign, DoNothing, If, Sequence, While
+        StatementMachine
+      else
+        raise "Unrecognized syntax: #{syntax.inspect}"
+      end
+
+    $stderr.puts "WARNING: Automatically using #{machine_class} instead of #{self}"
+
+    machine_class.new(syntax, *args)
+  end
+end
