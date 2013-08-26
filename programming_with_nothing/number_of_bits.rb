243abade15f3faf54828919e7f56e3210fe28a3c
Joel Chippindale <joel@joelchippindale.com>
2013-08-26 23:41:22 +0100

    Calculate the number of bits in a number
    Using lambda calculus

---
+++ programming_with_nothing/number_of_bits.rb
@@ -0,0 +1,108 @@
+# Calculate the number of bits in a number using lamdba calculus
+
+# Ruby helpers for translating output
+def to_integer(proc)
+  proc[-> n { n + 1 }][0]
+end
+
+def to_boolean(proc)
+  proc[true][false]
+end
+
+# Some numbers
+ZERO    = -> p { -> x {       x    } }
+ONE     = -> p { -> x {     p[x]   } }
+TWO     = -> p { -> x {   p[p[x]]  } }
+THREE   = -> p { -> x { p[p[p[x]]] } }
+FIVE    = -> p { -> x { p[p[p[p[p[x]]]]] } }
+FIFTEEN = -> p { -> x { p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[x]]]]]]]]]]]]]]] } }
+HUNDRED = -> p { -> x { p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[x]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]] } }
+
+# THE SOLUTION
+#
+# First in ruby to give a pattern to follow
+#
+#   def number_of_bits(x)
+#     if x == 0
+#       0
+#     else
+#       1 + number_of_bits(x/2)
+#     end
+#   end
+
+# Then some intemediate results
+TRUE = -> x { -> y { x } }
+FALSE = -> x { -> y { y } }
+IS_ZERO = -> n { n[ -> x { FALSE }][TRUE] }
+IF = -> b { b }
+
+INCREMENT = -> n { -> p { -> x { p[n[p][x]] } } }
+PAIR = -> x { -> y { -> f { f[x][y] } } }
+LEFT = -> p { p[ -> x { -> y { x } }] }
+RIGHT = -> p { p[ -> x { -> y { y } }] }
+SLIDE = -> p { PAIR[RIGHT[p]][INCREMENT[RIGHT[p]]] }
+DECREMENT = -> n { LEFT[n[SLIDE][PAIR[ZERO][ZERO]]] }
+Z = -> f { -> x { f[-> y { x[x][y] }] }[-> x { f[-> y { x[x][y] }] }] }
+IS_ONE = -> n { IS_ZERO[DECREMENT[n]] }
+DIVIDE_BY_TWO = Z[-> f { -> n { IF[IS_ZERO][n][
+    ZERO
+  ][IF[IS_ONE][n][
+    ZERO
+  ][
+    INCREMENT[-> x { f[DECREMENT[DECREMENT[n]]][x] }]
+  ]]
+} } ]
+
+# THE SOLUTION - in lambda calculus
+NUMBER_OF_BITS = Z[-> f { -> n { IF[IS_ZERO[n][
+    ZERO
+  ][
+    INCREMENT[-> x { f[DIVIDE_BY_TWO[n]][x] }]
+  ]]
+} } ]
+
+describe 'to_integer' do
+  it { to_integer(ZERO).should == 0 }
+  it { to_integer(ONE).should == 1 }
+end
+
+describe 'to_boolean' do
+  it { to_boolean(TRUE).should == true }
+  it { to_boolean(FALSE).should == false }
+end
+
+describe 'IS_ZERO' do
+  it { to_boolean(IS_ZERO[ZERO]).should == true }
+  it { to_boolean(IS_ZERO[ONE]).should == false }
+end
+
+describe 'INCREMENT' do
+  it { to_integer(INCREMENT[ZERO]).should == 1 }
+  it { to_integer(INCREMENT[ONE]).should == 2 }
+  it { to_integer(INCREMENT[TWO]).should == 3 }
+end
+
+describe 'DECREMENT' do
+  it { to_integer(DECREMENT[ZERO]).should == 0 }
+  it { to_integer(DECREMENT[ONE]).should == 0 }
+  it { to_integer(DECREMENT[TWO]).should == 1 }
+end
+
+describe 'DIVIDE_BY_TWO' do
+  it { to_integer(DIVIDE_BY_TWO[ZERO]).should == 0  }
+  it { to_integer(DIVIDE_BY_TWO[ONE]).should == 0  }
+  it { to_integer(DIVIDE_BY_TWO[TWO]).should == 1  }
+  it { to_integer(DIVIDE_BY_TWO[THREE]).should == 1  }
+  it { to_integer(DIVIDE_BY_TWO[HUNDRED]).should == 50  }
+end
+
+describe 'NUMBER_OF_BITS' do
+  it { to_integer(NUMBER_OF_BITS[ZERO]).should == 0 }
+  it { to_integer(NUMBER_OF_BITS[ONE]).should == 1 }
+  it { to_integer(NUMBER_OF_BITS[TWO]).should == 2 }
+  it { to_integer(NUMBER_OF_BITS[THREE]).should == 2 }
+  it { to_integer(NUMBER_OF_BITS[FIVE]).should == 3 }
+  it { to_integer(NUMBER_OF_BITS[FIFTEEN]).should == 4 }
+  it { to_integer(NUMBER_OF_BITS[HUNDRED]).should == 7 }
+end
+
